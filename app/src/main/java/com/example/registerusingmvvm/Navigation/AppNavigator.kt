5e018824c95e14b92e6a895099baf599745aa789
shouqalbdrani <shouqalbdrani@gmail.com>
2025-03-19 00:09:38 +0300

    Update AppNavigator.kt

--- app/src/main/java/com/example/registerusingmvvm/Navigation/AppNavigator.kt
+++ app/src/main/java/com/example/registerusingmvvm/Navigation/AppNavigator.kt
@@ -1,36 +1,3 @@
-//package com.example.registerusingmvvm.Navigation
-//
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
-//import androidx.navigation.compose.NavHost
-//import androidx.navigation.compose.composable
-//import androidx.navigation.compose.rememberNavController
-//import com.example.registerusingmvvm.view.SignInScreen
-//import com.example.registerusingmvvm.view.SignSuccessfully
-//import com.example.registerusingmvvm.view.SignUpScreen
-//import com.example.registerusingmvvm.viewmodel.AuthenticationViewModel
-//
-//@Composable
-//fun AppNavigation(modifier: Modifier = Modifier) {
-//    val navController = rememberNavController()
-//
-//    NavHost(
-//        navController = navController,
-//        startDestination = "sign_in",
-//        modifier = modifier
-//    ) {
-//        composable("sign_in") {
-//            SignInScreen(viewModel = AuthenticationViewModel(), navController = navController)
-//        }
-//        composable("sign_up") {
-//            SignUpScreen(viewModel = AuthenticationViewModel(), navController = navController)
-//        }
-//        composable("sign_successfully") {  // Fixed route syntax
-//            SignSuccessfully(navController)
-//        }
-//    }
-//}
-
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
